f26a58f1456248ecbd7794197089b1d060116c27
TrungTran <trungtran470@gmail.com>
2024-03-07 10:27:46 +0700

    update by Trung

--- idex.php
+++ idex.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
+    <?php
     echo "hello word "
     ?>
+    <h1>Hi Am TrUnG</h1>
 </body>
+
 </html>
